d892b0129e9d22f5001bc704755d0258ce7bc451
sanjee <79707868+sanjeevan1@users.noreply.github.com>
2023-09-24 07:14:50 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,62 @@
+import React , {useState , useEffect} from 'react'
+import axios from 'axios'
+import './App.css'
+import Addtask from './Components/Addtask'
+import Todolist from './Components/Todolist'
+import Updatetask from './Components/Updatetask'
+function App() {
+
+  const [todolist,setTodolist] = useState([])
+  const [tasktoUpdate , setTasktoUpdate] = useState({})
+  const [showPopup,setShowPopup] = useState(false)
+
+  // fetching data
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/tasks').then(res => {
+      setTodolist(res.data);
+      // console.log(res.data)
+    }).catch(err => console.log(err))
+  },[])
+
+
+// inset data aother page
+  const addTask = newTask => {
+    // settod list datavum ipo addd panura datavum
+    setTodolist([...todolist,newTask])
+  }
+  
+  const taskComplete = task => {
+    const newList = [...todolist]
+    newList.forEach(item => {
+      if(item._id === task._id){
+        item.isComplete = task.isComplete
+      }
+    })
+    setTodolist(newList)
+  }
+  const removeTask = task => {
+    const newList = todolist.filter(item => !(item._id === task._id))
+    setTodolist(newList)
+  }
+  const updatetask = task => {
+    const newList = [...todolist]
+    newList.forEach(item => {
+      if(item._id === task._id){
+        item.todo = task.todo
+      }
+    })
+    setTodolist(newList)
+  }
+
+  return (
+    <div>
+      {/* props sharing */}
+      <Addtask addTask = {addTask}/>
+      
+      <Todolist todolist = {todolist} taskComplete = {taskComplete} removeTask = {removeTask} tasktoUpdate = {task => setTasktoUpdate(task)} showPopup = {() => setShowPopup(!showPopup)}/>
+      {showPopup && <Updatetask task = {tasktoUpdate} updatetask = {updatetask} removePopup = {() => setShowPopup(!showPopup)}/>}
+    </div>
+  )
+}
+
+export default App
